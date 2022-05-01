bceeab047ddba89883c964b6a64c8a82b6ec9170
Flutter PC <sina.sam@gmail.com>
2022-05-01 23:00:46 +0430

    working on life balance lables

--- lib/screens/life_balance.dart
+++ lib/screens/life_balance.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:todoey/utils/constants.dart';
 import 'dart:math';
+import 'add_task_screen.dart';
 
 double value1 = 0.0;
 double value2 = 0.0;
@@ -10,6 +12,15 @@ double value5 = 0.0;
 double value6 = 0.0;
 double value7 = 0.0;
 double value8 = 0.0;
+
+String lable1 = 'سلامتی';
+String lable2 = 'دین';
+String lable3 = 'تفریح';
+String lable4 = 'پول';
+String lable5 = 'درس';
+String lable6 = 'عشق';
+String lable7 = 'خانواده';
+String lable8 = 'رشد';
 
 class lifeBalanceScreen extends StatefulWidget {
   const lifeBalanceScreen({Key? key}) : super(key: key);
@@ -62,77 +73,144 @@ class _lifeBalanceScreenState extends State<lifeBalanceScreen> {
                   ),
                 ],
               ),
-              arcSlider(
-                valueGiven: value1,
-                color: Colors.greenAccent,
-                onchange: (value) {
-                  setState(() {
-                    value1 = value;
-                  });
-                },
-              ),
-              arcSlider(
-                valueGiven: value2,
-                color: Colors.teal,
-                onchange: (value) {
-                  setState(() {
-                    value2 = value;
-                  });
-                },
-              ),
-              arcSlider(
-                valueGiven: value3,
-                color: Colors.cyan,
-                onchange: (value) {
-                  setState(() {
-                    value3 = value;
-                  });
-                },
-              ),
-              arcSlider(
-                valueGiven: value4,
-                color: Colors.indigo,
-                onchange: (value) {
-                  setState(() {
-                    value4 = value;
-                  });
-                },
-              ),
-              arcSlider(
-                valueGiven: value5,
-                color: Colors.deepPurple,
-                onchange: (value) {
-                  setState(() {
-                    value5 = value;
-                  });
-                },
-              ),
-              arcSlider(
-                valueGiven: value6,
-                color: Colors.purple,
-                onchange: (value) {
-                  setState(() {
-                    value6 = value;
-                  });
-                },
-              ),
-              arcSlider(
-                valueGiven: value7,
-                color: Colors.pinkAccent,
-                onchange: (value) {
-                  setState(() {
-                    value7 = value;
-                  });
-                },
-              ),
-              arcSlider(
-                valueGiven: value8,
-                color: Colors.pink,
-                onchange: (value) {
-                  setState(() {
-                    value8 = value;
-                  });
-                },
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 9),
+                child: Column(
+                  children: [
+                    arcSlider(
+                      onLablePress: () {
+                        setState(() {
+                          lable1 = 'value';
+                        });
+                      },
+                      silderLable: lable1,
+                      valueGiven: value1,
+                      color: Colors.greenAccent,
+                      onchange: (value) {
+                        setState(() {
+                          value1 = value;
+                        });
+                      },
+                    ),
+                    arcSlider(
+                      onLablePress: () {
+                        setState(() {
+                          showModalBottomSheet(
+                            isScrollControlled: true,
+                            context: context,
+                            builder: (context) => SingleChildScrollView(
+                              child: Container(
+                                padding: EdgeInsets.only(
+                                    bottom: MediaQuery.of(context)
+                                        .viewInsets
+                                        .bottom),
+                                child: EditLable(a: lable2),
+                              ),
+                            ),
+                          );
+                        });
+                      },
+                      silderLable: lable2,
+                      valueGiven: value2,
+                      color: Colors.teal,
+                      onchange: (value) {
+                        setState(() {
+                          value2 = value;
+                        });
+                      },
+                    ),
+                    arcSlider(
+                      onLablePress: () {
+                        setState(() {
+                          lable1 = 'value';
+                        });
+                      },
+                      silderLable: lable3,
+                      valueGiven: value3,
+                      color: Colors.cyan,
+                      onchange: (value) {
+                        setState(() {
+                          value3 = value;
+                        });
+                      },
+                    ),
+                    arcSlider(
+                      onLablePress: () {
+                        setState(() {
+                          lable1 = 'value';
+                        });
+                      },
+                      silderLable: lable4,
+                      valueGiven: value4,
+                      color: Colors.indigo,
+                      onchange: (value) {
+                        setState(() {
+                          value4 = value;
+                        });
+                      },
+                    ),
+                    arcSlider(
+                      onLablePress: () {
+                        setState(() {
+                          lable1 = 'value';
+                        });
+                      },
+                      silderLable: lable5,
+                      valueGiven: value5,
+                      color: Colors.deepPurple,
+                      onchange: (value) {
+                        setState(() {
+                          value5 = value;
+                        });
+                      },
+                    ),
+                    arcSlider(
+                      onLablePress: () {
+                        setState(() {
+                          lable1 = 'value';
+                        });
+                      },
+                      silderLable: lable6,
+                      valueGiven: value6,
+                      color: Colors.purple,
+                      onchange: (value) {
+                        setState(() {
+                          value6 = value;
+                        });
+                      },
+                    ),
+                    arcSlider(
+                      onLablePress: () {
+                        setState(() {
+                          lable1 = 'value';
+                        });
+                      },
+                      silderLable: lable7,
+                      valueGiven: value7,
+                      color: Colors.pink,
+                      onchange: (value) {
+                        setState(() {
+                          value7 = value;
+                        });
+                      },
+                    ),
+                    arcSlider(
+                      onLablePress: () {
+                        setState(() {
+                          lable1 = 'value';
+                        });
+                      },
+                      silderLable: lable8,
+                      valueGiven: value8,
+                      color: Colors.pinkAccent,
+                      onchange: (value) {
+                        setState(() {
+                          value8 = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
@@ -146,34 +224,117 @@ class arcSlider extends StatelessWidget {
   final void Function(double)? onchange;
   final color;
   final valueGiven;
+  final String silderLable;
+  final void Function()? onLablePress;
 
   const arcSlider({
     required this.onchange,
     required this.color,
     required this.valueGiven,
+    required this.silderLable,
+    required this.onLablePress,
   });
 
   @override
   Widget build(BuildContext context) {
     final myTextController = TextEditingController();
-    return Column(
+    return Row(
       children: [
-        TextField(
-          textAlign: TextAlign.center,
-          controller: myTextController,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
+        Expanded(
+          child: Slider(
+              activeColor: color,
+              label: valueGiven.toString(),
+              divisions: 20,
+              min: 0,
+              max: 10,
+              value: valueGiven,
+              onChanged: onchange),
+        ),
+        Container(
+          child: TextButton(
+            onPressed: onLablePress,
+            child: Text(
+              silderLable,
+              style: GoogleFonts.harmattan(
+                textStyle: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+              textAlign: TextAlign.center,
+            ),
           ),
         ),
-        Slider(
-            activeColor: color,
-            label: valueGiven.toString(),
-            divisions: 20,
-            min: 0,
-            max: 10,
-            value: valueGiven,
-            onChanged: onchange),
       ],
+    );
+  }
+}
+
+class EditLable extends StatelessWidget {
+  final a;
+  const EditLable({required this.a});
+
+  @override
+  Widget build(BuildContext context) {
+    String? newLable;
+    return Container(
+      color: Theme.of(context).colorScheme.background,
+      child: Container(
+        padding: EdgeInsets.only(
+          left: 40,
+          right: 40,
+          top: 20,
+        ),
+        decoration: BoxDecoration(
+          // color: Theme.of(context).colorScheme.onSecondary,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'چه کار دیگری باید انجام شود؟',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.lemonada(
+                  textStyle: TextStyle(
+                fontSize: 15,
+                color: Theme.of(context).colorScheme.onSecondary,
+                fontWeight: FontWeight.w600,
+              )),
+            ),
+            TextField(
+              onChanged: (e) {
+                newLable = e;
+                print(e);
+              },
+              textAlign: TextAlign.center,
+              autofocus: true,
+            ),
+            FlatButton(
+              color: Theme.of(context).colorScheme.secondary,
+              onPressed: () {
+                lable2 = newLable!;
+                // addNewTaskcallback(newTask);
+                // context.read<TaskData>().addTask(newTask!, user_id, false);
+                // context.read<TaskData>().addCloudTasksToList(user_id);
+                Navigator.pop(context);
+              },
+              child: Text(
+                'اضافه کن',
+                style: GoogleFonts.lemonada(
+                  textStyle: TextStyle(
+                    fontSize: 15,
+                    color: Theme.of(context).colorScheme.onSecondary,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -330,6 +491,32 @@ class LinePainter extends CustomPainter {
 
     canvas.drawPath(path7, paint7);
     canvas.drawPath(path8, paint8);
+
+    void arcLable(var varName, int arcStart, arcName) {
+      final textPainter = TextPainter(
+          text: TextSpan(
+              text: '$arcName',
+              style: GoogleFonts.harmattan(
+                  textStyle: TextStyle(fontSize: varName * 2))),
+          textDirection: TextDirection.rtl);
+      textPainter.layout(minWidth: 0, maxWidth: size.width);
+
+      final xCenter = (size.width - textPainter.width) / 2;
+      final yCenter = (size.height - textPainter.height) / 2;
+      final df = Offset.fromDirection(degToRad(arcStart - 22.5), varName * 10);
+      final offset = Offset(xCenter, yCenter) + df;
+
+      textPainter.paint(canvas, offset);
+    }
+
+    arcLable(value1, 0, lable1);
+    arcLable(value2, -45, lable2);
+    arcLable(value3, -90, lable3);
+    arcLable(value4, -135, lable4);
+    arcLable(value5, -180, lable5);
+    arcLable(value6, -225, lable6);
+    arcLable(value7, -270, lable7);
+    arcLable(value8, -315, lable8);
   }
 
   @override
