01c42555870b8e16ae7c53ed67cc36121ab0b65c
Flutter PC <sina.sam@gmail.com>
2022-05-07 03:05:19 +0430

    we have a timer!

--- lib/screens/pomodoro_screen.dart
+++ lib/screens/pomodoro_screen.dart
@@ -2,16 +2,194 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:todoey/utils/constants.dart';
 import 'dart:math';
+import 'dart:async';
 
-bool countDown = true;
-
-class PomodoroScreen extends StatelessWidget {
+class PomodoroScreen extends StatefulWidget {
   const PomodoroScreen({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<PomodoroScreen> createState() => _PomodoroScreenState();
+}
+
+class _PomodoroScreenState extends State<PomodoroScreen> {
+  static const countdownDuration = Duration(minutes: 25);
+  Duration duration = Duration();
+  Timer? timer;
+
+  bool countDown = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    reset();
+  }
+
+  void reset() {
+    if (countDown) {
+      setState(() => duration = countdownDuration);
+    } else {
+      setState(() => duration = Duration());
+    }
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (_) => addTime());
+  }
+
+  void addTime() {
+    final addSeconds = countDown ? -1 : 1;
+    setState(() {
+      final seconds = duration.inSeconds + addSeconds;
+      if (seconds < 0) {
+        timer?.cancel();
+      } else {
+        duration = Duration(seconds: seconds);
+      }
+    });
+  }
+
+  void stopTimer({bool resets = true}) {
+    if (resets) {
+      reset();
+    }
+    setState(() => timer?.cancel());
+  }
+
+////////////////////////////////
+
+//   @override
+//   Widget build(BuildContext context) => Scaffold(
+//         backgroundColor: Colors.orange[50],
+//         appBar: AppBar(
+//           automaticallyImplyLeading: false,
+//           title: Text("Flutter StopWatch Timer Demo"),
+//         ),
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               buildTime(),
+//               SizedBox(
+//                 height: 80,
+//               ),
+//               buildButtons()
+//             ],
+//           ),
+//         ),
+//       );
+//
+//   Widget buildTime() {
+//     String twoDigits(int n) => n.toString().padLeft(2, '0');
+//     final hours = twoDigits(duration.inHours);
+//     final minutes = twoDigits(duration.inMinutes.remainder(60));
+//     final seconds = twoDigits(duration.inSeconds.remainder(60));
+//     return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+//       buildTimeCard(time: hours, header: 'HOURS'),
+//       SizedBox(
+//         width: 8,
+//       ),
+//       buildTimeCard(time: minutes, header: 'MINUTES'),
+//       SizedBox(
+//         width: 8,
+//       ),
+//       buildTimeCard(time: seconds, header: 'SECONDS'),
+//     ]);
+//   }
+//
+//   Widget buildTimeCard({required String time, required String header}) =>
+//       Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Container(
+//             padding: EdgeInsets.all(8),
+//             decoration: BoxDecoration(
+//                 color: Colors.white, borderRadius: BorderRadius.circular(20)),
+//             child: Text(
+//               time,
+//               style: TextStyle(
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.black,
+//                   fontSize: 50),
+//             ),
+//           ),
+//           SizedBox(
+//             height: 24,
+//           ),
+//           Text(header, style: TextStyle(color: Colors.black45)),
+//         ],
+//       );
+//
+//   Widget buildButtons() {
+//     final isRunning = timer == null ? false : timer!.isActive;
+//     final isCompleted = duration.inSeconds == 0;
+//     return isRunning || isCompleted
+//         ? Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               ButtonWidget(
+//                   text: 'STOP',
+//                   onClicked: () {
+//                     if (isRunning) {
+//                       stopTimer(resets: false);
+//                     }
+//                   }),
+//               SizedBox(
+//                 width: 12,
+//               ),
+//               ButtonWidget(text: "CANCEL", onClicked: stopTimer),
+//             ],
+//           )
+//         : ButtonWidget(
+//             text: "Start Timer!",
+//             color: Colors.black,
+//             backgroundColor: Colors.white,
+//             onClicked: () {
+//               startTimer();
+//             });
+//   }
+// }
+//
+// class ButtonWidget extends StatelessWidget {
+//   final String text;
+//   final Color color;
+//   final Color backgroundColor;
+//   final VoidCallback onClicked;
+//
+//   const ButtonWidget(
+//       {Key? key,
+//       required this.text,
+//       required this.onClicked,
+//       this.color = Colors.white,
+//       this.backgroundColor = Colors.black})
+//       : super(key: key);
+//   @override
+//   Widget build(BuildContext context) => ElevatedButton(
+//       style: ElevatedButton.styleFrom(
+//           primary: backgroundColor,
+//           padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16)),
+//       onPressed: onClicked,
+//       child: Text(
+//         text,
+//         style: TextStyle(fontSize: 20, color: color),
+//       ));
+// }
+
+//////////////////////////////////////////////////
+
+  @override
   Widget build(BuildContext context) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = twoDigits(duration.inHours);
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+
+    final isRunning = timer == null ? false : timer!.isActive;
+    final isCompleted = duration.inSeconds == 0;
+// return isRunning || isCompleted
+    print('isRunning: $isRunning');
     return Scaffold(
       // appBar: GradiantAppBar(pageTitle: 'پومودورو'),
       body: Container(
@@ -54,9 +232,9 @@ class PomodoroScreen extends StatelessWidget {
                       ),
                     ),
                     Container(
-                      child: const Text(
-                        '12:36',
-                        style: TextStyle(
+                      child: Text(
+                        '$minutes:$seconds',
+                        style: const TextStyle(
                             color: Colors.teal,
                             fontSize: 50,
                             fontWeight: FontWeight.w900,
@@ -97,14 +275,32 @@ class PomodoroScreen extends StatelessWidget {
                         ],
                       ),
                     ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Icon(
-                        Icons.pause,
-                        size: 50,
-                        color: Colors.teal[700],
-                      ),
-                    ),
+                    //
+                    isRunning || isCompleted
+                        ? TextButton(
+                            onPressed: () {
+                              print('1');
+                              if (isRunning) {
+                                stopTimer(resets: false);
+                              }
+                            },
+                            child: Icon(
+                              Icons.pause,
+                              size: 50,
+                              color: Colors.teal[700],
+                            ),
+                          )
+                        : TextButton(
+                            onPressed: () {
+                              print('2');
+                              startTimer();
+                            },
+                            child: Icon(
+                              Icons.play_arrow,
+                              size: 50,
+                              color: Colors.teal[700],
+                            ),
+                          ),
                   ],
                 ),
                 SizedBox(height: 20),
