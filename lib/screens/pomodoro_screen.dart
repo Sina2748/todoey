60cdf24bc7b9df5aab1da26294b4ecbb0cf39c08
Flutter PC <sina.sam@gmail.com>
2022-05-07 01:10:44 +0430

    pomodoro layout 2.0

--- lib/screens/pomodoro_screen.dart
+++ lib/screens/pomodoro_screen.dart
@@ -3,81 +3,87 @@ import 'package:flutter/material.dart';
 import 'package:todoey/utils/constants.dart';
 import 'dart:math';
 
+bool countDown = true;
+
 class PomodoroScreen extends StatelessWidget {
-  const PomodoroScreen({Key? key}) : super(key: key);
+  const PomodoroScreen({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: GradiantAppBar(pageTitle: 'پومودورو'),
+      // appBar: GradiantAppBar(pageTitle: 'پومودورو'),
       body: Container(
+        height: double.infinity,
         width: double.infinity,
         decoration: kBackgroundImage,
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(6),
-          child: Column(
-            children: [
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  //---- Shadow
-                  Container(
-                    width: 330,
-                    height: 330,
-                    decoration: BoxDecoration(
-                        color: Colors.white.withOpacity(0.2),
-                        shape: BoxShape.circle),
-                  ),
-                  //---- Painting the Arc
-                  Container(
-                    width: 300,
-                    height: 300,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        shape: BoxShape.circle,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.3),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: Offset(-2, 4),
-                          ),
-                        ]),
-                    child: CustomPaint(
-                      painter: ClockPainter(),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(12),
+            child: Column(
+              children: [
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    //---- Shadow
+                    Container(
+                      width: 330,
+                      height: 330,
+                      decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.2),
+                          shape: BoxShape.circle),
                     ),
-                  ),
-                  Container(
-                    child: Text(
-                      '12:36',
-                      style: TextStyle(
-                          color: Colors.teal,
-                          fontSize: 50,
-                          fontWeight: FontWeight.w900,
-                          letterSpacing: 10),
+                    //---- Painting the Arc
+                    Container(
+                      width: 300,
+                      height: 300,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: Offset(-2, 4),
+                            ),
+                          ]),
+                      child: CustomPaint(
+                        painter: ClockPainter(),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  //---- Shadow
-                  Container(
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                        color: Colors.tealAccent.withOpacity(0.2),
-                        shape: BoxShape.circle),
-                  ),
-                  Container(
-                    width: 90,
-                    height: 90,
-                    decoration: BoxDecoration(
-                        gradient: RadialGradient(
+                    Container(
+                      child: const Text(
+                        '12:36',
+                        style: TextStyle(
+                            color: Colors.teal,
+                            fontSize: 50,
+                            fontWeight: FontWeight.w900,
+                            letterSpacing: 10),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    //---- Shadow
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.tealAccent.withOpacity(0.2),
+                          shape: BoxShape.circle),
+                    ),
+                    Container(
+                      width: 90,
+                      height: 90,
+                      decoration: BoxDecoration(
+                        gradient: const RadialGradient(
                           center: Alignment(0.05, -0.08),
-                          colors: [Colors.white, Color(0xfff1f1f1)],
+                          colors: [Colors.white, Color(0xfff8f8f8)],
                           stops: [0.5, 0.9],
                         ),
                         shape: BoxShape.circle,
@@ -88,97 +94,116 @@ class PomodoroScreen extends StatelessWidget {
                             blurRadius: 7,
                             offset: Offset(-2, 4),
                           ),
-                        ]),
-                    child: CustomPaint(
-                      painter: ClockPainter(),
+                        ],
+                      ),
                     ),
-                  ),
-                  Container(
-                    child: Icon(
-                      Icons.pause,
-                      color: Colors.teal[700],
+                    TextButton(
+                      onPressed: () {},
+                      child: Icon(
+                        Icons.pause,
+                        size: 50,
+                        color: Colors.teal[700],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              Row(
-                children: [
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      //---- Shadow
-                      Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                            color: Colors.white.withOpacity(0.2),
-                            shape: BoxShape.rectangle),
-                      ),
-                      Container(
-                        width: 70,
-                        height: 70,
-                        decoration: BoxDecoration(
-                            gradient: const LinearGradient(
-                              begin: Alignment.topRight,
-                              colors: [Colors.white, Color(0xfff1f1f1)],
-                              stops: [0.1, 0.9],
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            //---- Shadow
+                            Container(
+                              width: 100,
+                              height: 100,
+                              decoration: BoxDecoration(
+                                  color: Colors.white.withOpacity(0.2),
+                                  shape: BoxShape.rectangle),
                             ),
-                            shape: BoxShape.rectangle,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.3),
-                                spreadRadius: 3,
-                                blurRadius: 5,
-                                offset: Offset(-1, 3),
+                            Container(
+                              width: 70,
+                              height: 70,
+                              decoration: BoxDecoration(
+                                  gradient: const LinearGradient(
+                                    begin: Alignment.topRight,
+                                    colors: [Colors.white, Color(0xfff1f1f1)],
+                                    stops: [0.1, 0.9],
+                                  ),
+                                  shape: BoxShape.rectangle,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.3),
+                                      spreadRadius: 3,
+                                      blurRadius: 5,
+                                      offset: Offset(-1, 3),
+                                    ),
+                                  ]),
+                              child: Icon(
+                                Icons.work,
+                                color: Colors.teal[700],
                               ),
-                            ]),
-                        child: Icon(
-                          Icons.add_box_rounded,
-                          color: Colors.teal[700],
-                        ),
-                      ),
-                    ],
-                  ),
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      //---- Shadow
-                      Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                            color: Colors.white.withOpacity(0.2),
-                            shape: BoxShape.rectangle),
-                      ),
-                      Container(
-                        width: 70,
-                        height: 70,
-                        decoration: BoxDecoration(
-                            gradient: const LinearGradient(
-                              begin: Alignment.topRight,
-                              colors: [Colors.white, Color(0xfff1f1f1)],
-                              stops: [0.1, 0.9],
                             ),
-                            shape: BoxShape.rectangle,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.3),
-                                spreadRadius: 3,
-                                blurRadius: 5,
-                                offset: Offset(-1, 3),
-                              ),
-                            ]),
-                        child: Icon(
-                          Icons.anchor_sharp,
-                          color: Colors.teal[700],
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              )
-            ],
+                        Text(
+                          '25 min',
+                          style:
+                              TextStyle(fontSize: 20, color: Colors.blueGrey),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            //---- Shadow
+                            Container(
+                              width: 100,
+                              height: 100,
+                              decoration: BoxDecoration(
+                                  color: Colors.white.withOpacity(0.2),
+                                  shape: BoxShape.rectangle),
+                            ),
+                            Container(
+                              width: 70,
+                              height: 70,
+                              decoration: BoxDecoration(
+                                  gradient: const LinearGradient(
+                                    begin: Alignment.topRight,
+                                    colors: [Colors.white, Color(0xfff1f1f1)],
+                                    stops: [0.1, 0.9],
+                                  ),
+                                  shape: BoxShape.rectangle,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.3),
+                                      spreadRadius: 3,
+                                      blurRadius: 5,
+                                      offset: Offset(-1, 3),
+                                    ),
+                                  ]),
+                              child: Icon(
+                                Icons.emoji_food_beverage,
+                                color: Colors.teal[700],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          '5 min',
+                          style:
+                              TextStyle(fontSize: 20, color: Colors.blueGrey),
+                        )
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
